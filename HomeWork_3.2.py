6fad064722045c7f2303e7408134c4e216e08bda
Vitaliy Dementev <butt_899@mail.ru>
2020-06-16 15:46:38 +0300

    Upgrade function

--- HomeWork_3.2.py
+++ HomeWork_3.2.py
@@ -7,24 +7,24 @@ upload_URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
 headers = {'Authorization': 'AgAAAAAjFoh4AADLW1oDtANzDEaMqnsSqmjbH5U'}
 
 
-def translate_it_de(text, to_lang):
-    with open('DE.txt') as de:
+def translate_it_de(from_file, to_file, to_lang):
+    with open(from_file) as de:
         original_file = de.read()
 
     params = {
         'key': API_KEY,
         'text': original_file,
-        'lang': 'ru'.format(to_lang),
+        'lang': to_lang.format(to_lang),
     }
 
     response = requests.get(URL, params=params)
     json_ = response.json()
 
-    with open('DE_translate.txt', 'w') as f:
+    with open(to_file, 'w') as f:
         write = f.write(''.join(json_['text']))
 
         params = {
-            'path': 'DE_translate.txt',
+            'path': to_file,
             'overwrite': True,
             'fields': ''
         }
@@ -32,71 +32,13 @@ def translate_it_de(text, to_lang):
     response = requests.get(upload_URL, headers=headers, params=params)
 
     resp_json = response.json()['href']
-    files = {'file': open('DE_translate.txt')}
-    requests.put(resp_json, files=files)
-
-
-def translate_it_es(text, to_lang):
-    with open('ES.txt') as es:
-        text = es.read()
-
-    params = {
-        'key': API_KEY,
-        'text': text,
-        'lang': 'ru'.format(to_lang),
-    }
-
-    response = requests.get(URL, params=params)
-    json_ = response.json()
-
-    with open('ES_translate.txt', 'w') as f:
-        write = f.write(''.join(json_['text']))
-
-    params = {
-        'path': 'ES_translate.txt',
-        'overwrite': True,
-        'fields': ''
-    }
-
-    response = requests.get(upload_URL, headers=headers, params=params)
-
-    resp_json = response.json()['href']
-    files = {'file': open('ES_translate.txt')}
-    requests.put(resp_json, files=files)
-
-
-def translate_it_fr(text, to_lang):
-    with open('FR.txt') as fr:
-        text = fr.read()
-
-    params = {
-        'key': API_KEY,
-        'text': text,
-        'lang': 'ru'.format(to_lang),
-    }
-
-    response = requests.get(URL, params=params)
-    json_ = response.json()
-
-    with open('FR_translate.txt', 'w') as f:
-        write = f.write(''.join(json_['text']))
-
-    params = {
-        'path': 'FR_translate.txt',
-        'overwrite': True,
-        'fields': ''
-    }
-
-    response = requests.get(upload_URL, headers=headers, params=params)
-
-    resp_json = response.json()['href']
-    files = {'file': open('FR_translate.txt')}
+    files = {'file': open(to_file)}
     requests.put(resp_json, files=files)
 
 
 if __name__ == '__main__':
-    translate_it_de('text', 'ru')
-    translate_it_es('text', 'ru')
-    translate_it_fr('text', 'ru')
+    translate_it_de('DE.txt', 'DE_translate.txt', 'ru')
+    translate_it_de('ES.txt', 'ES_translate.txt', 'ru')
+    translate_it_de('FR.txt', 'FR_translate.txt', 'ru')
 
 
